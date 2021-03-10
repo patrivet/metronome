f828908600c48dc1c4e9ccd3b945476865a88b0e
Pat Rivet <patrickmrivet@gmail.com>
2021-03-10 11:06:33 +0000

    Fix: songs not displaying

--- src/components/Songs/Songs.js
+++ src/components/Songs/Songs.js
@@ -9,12 +9,15 @@ const Songs = () => {
   const { activeBpm } = useContext(AppContext);
   const bpmSongs = () => {
     // filter songs matching activeBpm only.
-    return allSongs.filter( ([song, artist, bpm]) => Number(bpm) === activeBpm)
+    allSongs.filter( ([name, artist, bpm]) => {
+      return Number(bpm) == activeBpm;
+    })
+    return allSongs.filter( ([name, artist, bpm]) => Number(bpm) == activeBpm)
   }
 
   return (
     <div className="songs">
-      <p>Songs that use this BPM:</p>
+      <p>Song that use this BPM: ({activeBpm})</p>
       <div className="songs__song">{bpmSongs().map( ([name, artist, bpm]) => {
         return (<p key={`${name}_${artist}_${bpm}`}>{name}{' '}({artist})</p>)
       })}
